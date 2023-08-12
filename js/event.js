d9940b9af4c01454cc7a70a6abdb462b462ff950
shakibnymr <shakibnymr11@gmail.com>
2023-08-13 00:11:50 +0600

    onclick confused function calling

--- js/event.js
+++ js/event.js
@@ -1,1 +1,18 @@
-console.log('this is separate JS file');
+console.log("this is separate JS file");
+// opt 1: directly set on the html element
+// onclick option 2
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+// option 3
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+// option 3 another
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple'
+}
