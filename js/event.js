59265d87412aca984a667feb81eebe3a54135276
shakibnymr <shakibnymr11@gmail.com>
2023-08-13 01:31:51 +0600

    vary

--- js/event.js
+++ js/event.js
@@ -1,6 +1,7 @@
 console.log("this is separate JS file");
 // opt 1: directly set on the html element
-// onclick option 2
+// onclick option 2: 
+// IMPORTANT: we will use this
 function makeRed() {
   document.body.style.backgroundColor = "red";
 }
@@ -11,8 +12,25 @@ makeBlueButton.onclick = makeBlue;
 function makeBlue() {
   document.body.style.backgroundColor = "blue";
 }
-// option 3 another
+// option 3 another 
 const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
     document.body.style.backgroundColor = 'purple'
 }
+// option 4
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click',makePink)
+
+function makePink (){
+  document.body.style.backgroundColor = 'pink';
+}
+// opt 4 another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click',function makeGreen(){
+ document.body.style.backgroundColor = 'green';
+})
+// opt 4 final
+// [we will use this sometimes]
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod'
+})
